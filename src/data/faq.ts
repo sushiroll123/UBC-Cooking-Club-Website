78d547e16595ece8daf337472a44371deae9c409
sushiroll <daniel.zhan2718@gmail.com>
2026-09-16 20:21:06 -0700

    comments

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -9,12 +9,12 @@ export const faqData: FAQ[] = [
     answer:
       "We are the UBC Cooking Club – a club that creates a welcoming environment and a safe space where individuals learn to cook and bake delicious and healthy foods. Throughout our workshops, you are able to pick up new recipes and meet new like-minded friends who are also interested in cooking or baking!",
   },
-  {
+  { // TODO
     question: "How much is membership?",
     answer:
       "Membership prices are $10 for one term (Sept - Dec 2024, or Jan - Apr 2025) and $17 for the entire year (Sept 2024 - Apr 2025). You can join through the link in our Instagram bio, or in-person at one of our events.",
   },
-  {
+  { // TODO
     question: "Why should you join?",
     answer:
       "By being a Cooking Club member, you would get discounted prices for workshops and be able to benefit from our year-round sponsors that are being added on an ongoing basis. The sponsor list will be updated on our Instagram or newsletter. You can sign up for our newsletter through the link on our Instagram bio too.",
